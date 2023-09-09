0633cee0632a524bf8857ac782710f2d916e29d6
Duy Nguyễn <104379213+Duy-07@users.noreply.github.com>
2023-09-09 11:36:31 +0700

    room

---
+++ routes/roomRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const roomCtl = require("../controllers/roomController");
+
+const router = express.Router();
+router.get("/rooms", roomCtl.getAllRooms);
+router.get("/room/:id", roomCtl.getRoomById);
+router.post("/room", roomCtl.createRoom);
+router.put("/room/:id", roomCtl.updateRoom);
+router.delete("/room/:id", roomCtl.deleteRoom);
+
+module.exports = router;
